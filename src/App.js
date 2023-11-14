f90700f3e05ce88373fd8d38d0c556515a5969cb
Ritesh Harel <ritesh.harel@mindtickle.com>
2023-11-14 13:16:08 +0530

    add project/blog

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Education from "./Education/Education";
 import Projects from "./Projects/Projects";
 import Footer from "./Footer/Footer";
 import Navbar from "./Navbar/Navbar";
-//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
+//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
 function App() {
     return (
         <div className="myApp">
